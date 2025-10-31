68a1584b601ba4e302d334c6279e2ecaf94c8bd0
/gadget_saavy <68850752+74Thirsty@users.noreply.github.com>
2025-10-31 06:19:59 +0000

    Wrap renderer in theme provider

--- renderer/src/main.tsx
+++ renderer/src/main.tsx
@@ -2,14 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, HashRouter } from 'react-router-dom';
 import App from './App';
+import { ThemeProvider } from './context/ThemeContext';
 import './styles.css';
 
 const Router = import.meta.env.DEV ? BrowserRouter : HashRouter;
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <ThemeProvider>
+      <Router>
+        <App />
+      </Router>
+    </ThemeProvider>
   </React.StrictMode>
 );
